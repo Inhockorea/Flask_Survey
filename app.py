9a67dfab37a7b955ac37d4e444e3b4bffeaa0d3f
Graham Trail <Graham@Grahams-MacBook-Pro.local>
2020-03-19 17:20:57 -0700

    multiple surveys

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import satisfaction_survey
+from surveys import surveys
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "Oh-itisa-secret"
@@ -9,10 +9,22 @@ app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 debug = DebugToolbarExtension(app)
 
 @app.route("/")
+def pick_survey():
+    "Page where the user selects the survey to answer"
+
+    return render_template("select_survey.html", surveys = surveys)
+
+@app.route("/welcome", methods=["POST"])
 def starting_survey():
     "Allows user to begin survey"
 
-    return render_template("start_survey.html", survey=satisfaction_survey)
+    selected_survey = request.form["survey_choice"]
+    survey = surveys[selected_survey]
+    session["active_survey"] = selected_survey
+
+    return render_template("start_survey.html", survey=survey)
+
+
 
 @app.route("/start", methods=["POST"])
 def start_survey():
@@ -28,8 +40,10 @@ def show_questions(num):
     """Shows the next unanswered question in the survey,
     if user tries to enter different question, redirects them"""
     
+    active_survey = surveys[session["active_survey"]]
+    
     if not int(num) == int(session["question_num"]):
-        if int(session["question_num"]) >= len(satisfaction_survey.questions):
+        if int(session["question_num"]) >= len(active_survey.questions):
             flash("YOU'VE ALREADY FINISHED")
             return redirect("/thankyou")
         else:
@@ -38,7 +52,7 @@ def show_questions(num):
             print("Flash message", "Rightnum", rightnum, "Num", num)
             return redirect(f"/questions/{rightnum}") 
 
-    question = satisfaction_survey.questions[int(num)]
+    question = active_survey.questions[int(num)]
     
     session["question_num"] = num
 
@@ -49,6 +63,8 @@ def show_questions(num):
 def handle_answer():
     "append answer to responses list and redirect to the next question"
 
+    active_survey = surveys[session["active_survey"]]
+
     #Seems to work in terminal, profs say no - why?
     session["answers"].append(request.form["choice"])
     
@@ -58,7 +74,7 @@ def handle_answer():
     print("Your answers so far", session["answers"])
 
     #May want to impliment an active survey variable?
-    if int(num) >= len(satisfaction_survey.questions):
+    if int(num) >= len(active_survey.questions):
         return redirect("/thankyou")
     else:
         return redirect(f"/questions/{num}") 
